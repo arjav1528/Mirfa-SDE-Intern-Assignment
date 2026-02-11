bf987a8100229526e09d6c2b013d5a2d6fdfbcbd
Arjav Patel <arjav1528@gmail.com>
2026-02-11 18:57:20 +0530

    req/res types

---
+++ packages/crypto/src/types.ts
@@ -0,0 +1,18 @@
+export interface EncryptInput {
+  partyId: string;
+  payload: object;
+}
+
+export interface TxSecureRecord {
+  id: string;
+  partyId: string;
+  createdAt: string;
+  payload_nonce: string;
+  payload_ct: string;
+  payload_tag: string;
+  dek_wrap_nonce: string;
+  dek_wrapped: string;
+  dek_wrap_tag: string;
+  alg: string;
+  mk_version: string;
+}
